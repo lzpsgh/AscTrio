bef9756f4fef1ec37c1a964707c17465bff5ca8a
lensaclrtn <lensaclrtn@gmail.com>
2021-04-30 21:36:50 +0800

    feat: 引入colorlog库支持日志颜色高亮
    Signed-off-by: lensaclrtn <lensaclrtn@gmail.com>

--- util/logger.py
+++ util/logger.py
@@ -2,6 +2,8 @@ import logging
 import os
 import time
 
+from colorlog import ColoredFormatter
+
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 # 定义日志文件路径
 LOG_PATH = os.path.join(BASE_PATH, "log")
@@ -26,15 +28,36 @@ class Logger:
         self.logger = logging.getLogger("log")
         self.logger.setLevel(logging.INFO)
 
-        fmt = '[%(asctime)s][%(filename)s %(lineno)d][%(levelname)s]: %(message)s'
-        self.formater = logging.Formatter(fmt)
+        # 设置输出的日志字符串格式
+        # fmt = '[%(asctime)s][%(filename)s %(lineno)d][%(levelname)s]: %(message)s'
+        LOG_FORMAT_CONSOLE = "%(log_color)s[%(asctime)s] [%(filename)s %(lineno)d][%(levelname)-5.5s] %(message)s"
+        LOG_FORMAT_FILE = "%(asctime)s[%(filename)s %(lineno)d][%(levelname)-5.5s] %(message)s"
 
+        # 设置颜色
+        # self.formater = logging.Formatter(LOG_FORMAT_CONSOLE)
+        formatter_file = logging.Formatter(LOG_FORMAT_FILE)
+        formatter_console = ColoredFormatter(
+            LOG_FORMAT_CONSOLE,
+            datefmt=None,
+            reset=True,
+            log_colors={
+                'DEBUG': 'white',
+                'INFO': 'cyan',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+            },
+            secondary_log_colors={},
+            style='%'
+        )
+
+        # 分别设置日志和控制台
         self.filelogger = logging.FileHandler(self.logname, mode='a', encoding="UTF-8")
+        self.filelogger.setLevel(logging.DEBUG)
+        self.filelogger.setFormatter(formatter_file)
         self.console = logging.StreamHandler()
         self.console.setLevel(logging.DEBUG)
-        self.filelogger.setLevel(logging.DEBUG)
-        self.filelogger.setFormatter(self.formater)
-        self.console.setFormatter(self.formater)
+        self.console.setFormatter(formatter_console)  # self.formater
+        # 分别添加到handler中
         self.logger.addHandler(self.filelogger)
         self.logger.addHandler(self.console)
 
