214aafa80700a58e7f08e8fe34847a5cb328e8a6
lensaclrtn <lensaclrtn@gmail.com>
2021-05-02 02:44:50 +0800

    feat: 合并other接口的service层和api层
    Signed-off-by: lensaclrtn <lensaclrtn@gmail.com>

--- api/other.py
+++ api/other.py
@@ -4,8 +4,8 @@
 
 from base.base_request import BaseRequest
 from config import envar
-
-api_root_url = envar.BASE_URL_CORE
+from util import auth
+from util import common
 
 
 class Other(BaseRequest):
@@ -14,14 +14,58 @@ class Other(BaseRequest):
         super(Other, self).__init__(api_root_url, **kwargs)
 
     # 给9.9元流量课用户 重新推送[新带新]链接
-    def send_paysucmsg_fast(self, **kwargs):
-        return self.request("GET", f"/wxmsg/sendPaySucMsgFast", **kwargs)
+    def send_paysucmsg_fast(self, phone):
+        self.req_body = {
+            "phone": phone
+        }
+        self.req_cookies = {
+            'JSESSIONID': auth.get_cookie('crm'),
+        }
+        result = self.request("GET", "/wxmsg/sendPaySucMsgFast",
+                              params=self.req_body,
+                              headers=self.req_headers,
+                              cookies=self.req_cookies)
+        common.result_check(result)
+        return result
 
-    def add_sprite(self, **kwargs):
-        return self.request("POST", "/mate/addSprite", **kwargs)
+    def add_sprite(self):
+        self.req_body = {
+            'isCommon': False,
+            'price': 8,
+            'spriteName': '环保比赛-孩子8',
+            'dataURL': 'https://res.miaocode.com/6ff0d74b-29e6-424f-8870-a08a6f58b995.png'
+        }
+        self.req_cookies = {
+            'JSESSIONID': auth.get_cookie('crm'),
+        }
+        result = self.request("POST", "/mate/addSprite",
+                              data=self.req_body,
+                              headers=self.req_headers,
+                              cookies=self.req_cookies)
+        common.result_check(result)
+        return result
 
-    def add_stage(self, **kwargs):
-        return self.request("POST", "/mate/addStage", **kwargs)
+    def add_stage(self):
+        self.req_body = {
+            'isCommon': False,
+            'comment': 'asdf',
+            'price': 0,
+            'stageName': '环保比赛-新几4',
+            'dataURL': 'https://res.miaocode.com/29fd099f-286d-42cc-99e7-44dcb330e4e6.jpg'
+        }
+        self.req_cookies = {
+            'JSESSIONID': auth.get_cookie('crm'),
+        }
+        result = self.request("POST", "/mate/addStage",
+                              data=self.req_body,
+                              headers=self.req_headers,
+                              cookies=self.req_cookies)
+        common.result_check(result)
+        return result
 
 
-other = Other(api_root_url)
+other = Other(envar.BASE_URL_CORE)
+
+if __name__ == '__main__':
+    # other.send_paysucmsg_fast("18899758128")
+    other.add_stage()
